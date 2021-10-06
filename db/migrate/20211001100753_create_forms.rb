60e776ee9354f06734fd3ce8ad2ab423294d7451
cassiano yasumitsu <cassiano@usuarioincomum.com>
2021-10-06 22:51:25 +0900

    init

---
+++ db/migrate/20211001100753_create_forms.rb
@@ -0,0 +1,20 @@
+class CreateForms < ActiveRecord::Migration[6.0]
+  def change
+    create_table :forms do |t|
+      t.string :name
+      t.string :phone
+      t.string :email
+      t.string :prefecture
+      t.text :address
+      t.boolean :driver
+      t.text :licences
+      t.string :passport
+      t.string :my_number
+      t.text :previous_job
+      t.text :job_kind
+      t.text :observations
+
+      t.timestamps
+    end
+  end
+end
